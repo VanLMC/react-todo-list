98fc342a41b4e907581ab2e87fc6f01523716514
Vanderlei <vanderleichagas42@gmail.com>
2020-05-22 17:31:20 -0300

    AddTodo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+    state = {
+        title: 'title'
+    }
+
+    onChange = (e) => this.setState({title: e.target.value})
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title)
+        this.setState({title: ''})
+    }
     render() {
         return (
             <div>
-                <form style={{display: 'flex'}}>
+                <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
                     <input 
                         type="text" 
                         name="title" 
                         style={{flex: '10', padding: '5px'}}
                         placeholder="Add Todo ..."
+                        value={this.state.title}
+                        onChange={this.onChange}
                     />
                     <input type="submit" value="Submit" className="btn" style={{flex: 1}}/>
                 </form>
